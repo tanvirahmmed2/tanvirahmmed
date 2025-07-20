24faac2698025de32f0fadce553b46bbe823e141
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-20 17:32:42 +0600

    update

--- src/Componenet/NavBar.jsx
+++ src/Componenet/NavBar.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
-import { FaBars } from "react-icons/fa";
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
 
 function NavBar() {
-    const [menu, setMenu] = useState("hidden");
+    const [menu, setMenu] = useState(false);
     const handlemenu = () => {
-        setMenu(menu === "hidden" ? "flex" : "hidden")
+        setMenu(!menu)
     }
 
     return (
@@ -16,13 +16,17 @@ function NavBar() {
             <a href="/" className='w-auto h-12 flex items-center justify-center  font-bold text-xl sm:text-3xl cursor-grab'>Tanvir Ahmmed</a>
 
             <div className={`relative flex md:hidden flex-col items-center justify-center h-12 w-28`}>
-                <p onClick={handlemenu} className='h-12 w-16 flex items-center justify-center cursor-pointer hover:border-b-2' ><FaBars/></p>
-                <div className={`absolute ${menu} flex-col gap-4 items-start top-12 shadow-2xl bg-white py-4 px-4 rounded-xl`}>
-                    <Link to='/services' className='w-[100px] h-8 items-center justify-center flex hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold '>Services</Link>
-                    <Link to='/projects' className='w-[100px] h-8 items-center justify-center flex hover:scale-110 hover:text-teal-500 cursor-pointer  font-semibold '>Projects</Link>
-                    <Link to='/reviews' className='w-[100px] h-8 items-center justify-center flex  hover:scale-110 hover:text-teal-500 cursor-pointer  font-semibold '>Reviews</Link>
-                    <Link to='/contact' className='w-[100px] h-8 items-center justify-center flex  hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold '>Contact</Link>
-                    <a href="/Tanvir_Ahmmed_resume.pdf"  download className='w-[100px] h-8 items-center justify-center flex  hover:scale-110 hover:text-teal-500 cursor-pointer   font-semibold text-red-600'>Resume</a>
+                <p onClick={handlemenu} className='h-12 w-16 flex gap-4 items-center justify-center cursor-pointer hover:border-b-2' >
+                    <p className={`text-3xl ${menu ? "hidden":"flex"}`}><HiBars3BottomLeft/></p>
+                    <p className={`text-3xl ${!menu ? "hidden":"flex"}`}><IoMdClose/></p>
+                    
+                </p>
+                <div className={`absolute ${!menu ? "hidden":"flex"} flex-col w-[130px] gap-4 items-start top-12 shadow-2xl bg-white py-4 px-4 rounded-xl`}>
+                    <Link to='/services' className=' h-8 w-full pl-4 hover:pl-6 hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold'>Services</Link>
+                    <Link to='/projects' className='h-8 w-full pl-4 hover:pl-6 hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold '>Projects</Link>
+                    <Link to='/reviews' className='h-8 w-full pl-4 hover:pl-6 hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold'>Reviews</Link>
+                    <Link to='/contact' className='h-8 w-full pl-4 hover:pl-6 hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold '>Contact</Link>
+                    <a href="/Tanvir_Ahmmed_resume.pdf"  download className='h-8 w-full pl-4 hover:pl-6 hover:scale-110 hover:text-teal-500 cursor-pointer font-semibold text-red-600'>Resume</a>
                 </div>
             </div>
 
