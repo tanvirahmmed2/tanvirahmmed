d1ca69ae48687af95d1b43640c23d3a36f9c60e8
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-21 17:03:04 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import About from './Componenet/About'
 
 function App() {
   return (
-    <div className='overflow-x-hidden w-full relative select-none h-auto bg-gray-100 font-manrope '>
+    <div className='overflow-x-hidden w-full relative select-none h-auto bg-gray-100 font-sans '>
 
       <NavBar />
       
