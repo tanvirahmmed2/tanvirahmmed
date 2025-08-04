ce62beb88eec671ca92fc535544fbd45bab624c2
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-04 23:14:28 +0600

    update

--- src/Componenet/Notice.jsx
+++ src/Componenet/Notice.jsx
@@ -17,13 +17,13 @@ const Notice = () => {
 
 
     return (
-        <div className={`w-full h-8 bg-black/20 ${notice ? "flex" : "hidden"} flex-row items-center justify-around gap-4 text-xs lg:text-base`}>
-            <p className='text-white/30 italic'>currently available for new job or project offer</p>
+        <div className={`w-full h-8 ${light? "bg-white": "bg-black/20"} border-[1px] ${notice ? "flex" : "hidden"} flex-row items-center justify-around gap-4 text-xs lg:text-base`}>
+            <p className='italic opacity-30'>currently available for new job or project offer</p>
             <span onClick={handlemood} className='w-auto h-8 flex items-center justify-center '>
                 <p className={`w-auto h-8 ${light? "flex": "hidden"} cursor-pointer rounded-xl gap-2 items-center justify-center px-3`}><IoMdSunny /> Light</p>
                 <p className={`w-auto h-8 ${light? "hidden": "flex"} cursor-pointer rounded-xl gap-2 items-center justify-center px-3`}><IoMdMoon /> Dark</p>
             </span>
-            <p onClick={handlenoticeboard} className='text-white hover:bg-white/5 cursor-pointer rounded-md p-2 '><IoMdClose /></p>
+            <p onClick={handlenoticeboard} className=' hover:bg-white/5 cursor-pointer rounded-md p-2 '><IoMdClose /></p>
         </div>
     )
 }
