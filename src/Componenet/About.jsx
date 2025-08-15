1d44796ec970d4bc0286985f2ba5d7f46c453300
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-15 18:44:56 +0600

    update

--- src/Componenet/About.jsx
+++ src/Componenet/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
         </motion.div>
 
 
-        <motion.div initial={{x:50, opacity:0}} whileInView={{x:0, opacity:1}} transition={{duration:0.6}} className='w-[400px] lg:w-[600px] h-auto flex flex-col items-center justify-center gap-8 text-center lg:text-start px-6'>
+        <motion.div initial={{x:50, opacity:0}} whileInView={{x:0, opacity:1}} transition={{duration:0.6}} className='w-3/4 lg:w-[600px] h-auto flex flex-col items-center justify-center gap-8 text-center lg:text-start px-6'>
           <div className='w-full h-auto flex flex-col items-start justify-center gap-4'>
             <h1 className='w-full h-auto text-center lg:text-start text-3xl' >Hello!</h1>
             <p>I'm <span className='font-bold text-xl'>Tanvir Ahmmed</span>, a passionate MERN stack developer who loves creating digital experiences that make a difference. With expertise in modern web technologies, I transform complex ideas into elegant, functional solutions.</p>
