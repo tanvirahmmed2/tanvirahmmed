39d9f0c6aaaf99691be2d816f4622516d272f1fb
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-27 19:59:17 +0600

    update

--- src/Componenet/layout/Home.jsx
+++ src/Componenet/layout/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Projects from '../pages/Projects'
 import Review from '../pages/Review'
-import Contact from '../pages/Contact'
 import About from '../pages/About'
 import UsePageTitle from '../UsePageTitle'
-import Skills from '../pages/Skills'
 import Intro from '../pages/Intro'
 
 
@@ -15,10 +12,7 @@ function Home() {
     <div className='w-full h-auto flex flex-col items-center justify-center '>
       <Intro />
       <About />
-      <Skills />
-      <Projects />
       <Review />
-      <Contact />
 
     </div>
   )
